050ffb3a7f7ef0078710f71728386c59f742fd21
Matiin05 <matiasnoya37@gmail.com>
2025-08-15 15:57:40 -0300

    configuracion de secretkey en app properties

--- backend/src/main/java/com/turnomatic/backend/repository/Usuario/UsuarioRepository.java
+++ backend/src/main/java/com/turnomatic/backend/repository/Usuario/UsuarioRepository.java
@@ -9,12 +9,15 @@ import java.util.Optional;
 import com.turnomatic.backend.model.Usuario.Rol;
 import java.util.Set;
 
-
-
 public interface UsuarioRepository extends JpaRepository<Usuario,Long>{
     Optional<Usuario> findByEmail(String email);
+
+    Boolean existsByEmail(String email);
     
     List<Usuario> findByRoles(Set<Rol> roles);
 
     List<Usuario> findBySucursalId(Long sucursalId);
+    
+    // Método para buscar usuarios por sucursal y negocio
+    Set<Usuario> findBySucursalIdAndNegocioId(Long sucursalId, Long negocioId);
 }
